dcd242f85d402600c6dce62014676d36f3cbaf94
Saysuu <skysaika@yandex.ru>
2023-03-14 16:40:42 +0300

    added settings with path

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -1,17 +1,22 @@
 import json
 import os
 
+from dotenv import load_dotenv
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
 import isodate
 
+from settings import ENV_FILE
+
+load_dotenv(ENV_FILE)
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
+api_key: str = os.getenv('API_KEY')
 
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 
 def printj(dict_to_print: dict) -> None:
